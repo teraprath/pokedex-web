c78ce68a95da94bcc579336c36ae8f97fbb03cbe
Terry <68700354+teraprath@users.noreply.github.com>
2021-12-05 21:29:53 +0100

    Base Version
    + Base Version

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-from os import name
-from flask import Flask, render_template, jsonify, make_response
+from logging import error
+from flask import Flask, render_template, jsonify, request
 import requests
-import json
-import random
 
 app = Flask(__name__)
 
-amount = 898
 api = "https://pokeapi.co/api/v2"
 
-@app.route("/<string:name>", methods=["POST", "GET"])
-def get(name):
-    url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+@app.route("/search/<string:name>", methods=["POST", "GET"])
+def search(name):
+    url = api + "/pokemon/" + name
     req = requests.get(url)
     res = req.json()
-    return jsonify(res)
+    return jsonify("", render_template("includes/card.html", res=res))
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    return render_template("pages/index.html", list=list)
+    pokemon = None
+    name = None
+    if request.method == "POST":
+        name = request.form["name"]
+        url = api + "/pokemon/" + name
+        try:
+            req = requests.get(url)
+            res = req.json()
+            pokemon = {}
+            pokemon["name"] = res["name"]
+            pokemon["id"] = "#" + str(res["id"])
+            pokemon["type"] = res["types"][0]["type"]["name"]
+            pokemon["height"] = res["height"]
+            pokemon["weight"] = res["weight"]
+            pokemon["sprite"] = res["sprites"]["other"]["dream_world"]["front_default"]
+        except:
+            pokemon = "Not Found"
+    return render_template("pages/index.html", pokemon=pokemon, name=name)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5500)
+    app.run(debug=True, port=5500) 
