b6f9fa295f4bad540189537cce6c57a5f932aa0c
teraprath <teraprath.thuansuriya@gmail.com>
2021-12-07 13:21:24 +0100

    Progress Bars
    + Progress Bars
    + Dicts

--- __init__.py
+++ __init__.py
@@ -9,22 +9,32 @@ def index():
     pokemon = None
     name = None
     if request.method == "POST":
-        name = request.form["name"].lower()
+        name = request.form["name"].lower().replace(" ", "-")
         url = api + "/pokemon/" + name
         try:
             req = requests.get(url)
             res = req.json()
             pokemon = {}
             pokemon["name"] = res["name"]
-            pokemon["id"] = res["id"]
+            
+            pokemon["info"] = {}
+            pokemon["info"]["id"] = "#" + str(res["id"])
+            pokemon["info"]["exp"] = res["base_experience"]
+            pokemon["info"]["height"] = str(res["height"]) + '"'
+            pokemon["info"]["weight"] = str(res["weight"]) + "kg"
+
             pokemon["types"] = []
             n = len(res["types"])
             for i in range(n):
                 type = res["types"][i]["type"]["name"]
                 pokemon["types"].append(type)
-            pokemon["height"] = res["height"]
-            pokemon["weight"] = res["weight"]
-            pokemon["base_exp"] = res["base_experience"]
+
+            pokemon["stats"] = {}
+            n = len(res["stats"])
+            for i in range(n):
+                j = res["stats"][i]["stat"]["name"]
+                pokemon["stats"][j] = res["stats"][i]["base_stat"]
+
             pokemon["sprite"] = res["sprites"]["other"]["dream_world"]["front_default"]
             if pokemon["sprite"] == None:
                 pokemon["sprite"] = res["sprites"]["front_default"]
@@ -33,4 +43,4 @@ def index():
     return render_template("pages/index.html", pokemon=pokemon, req=name)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5500) 
+    app.run(debug=True, port=5500)
