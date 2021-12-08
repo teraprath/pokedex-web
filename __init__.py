da8fa54c7d2135c1e7fb92ad83efe037785f195e
teraprath <teraprath.thuansuriya@gmail.com>
2021-12-08 14:13:40 +0100

    Stats
    + Stats

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, jsonify, request
-import requests
+from flask import Flask, render_template, request
+from requests.api import get
+import pokeapi
 
 app = Flask(__name__)
-api = "https://pokeapi.co/api/v2"
+
+@app.route("/stats/<string:pokemon>")
+def stats(pokemon):
+    name = pokemon.lower()
+    pokemon = pokeapi.getData(name)
+    return render_template("pages/stats.html",  pokemon=pokemon)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -10,37 +16,9 @@ def index():
     name = None
     if request.method == "POST":
         name = request.form["name"].lower().replace(" ", "-")
-        url = api + "/pokemon/" + name
-        try:
-            req = requests.get(url)
-            res = req.json()
-            pokemon = {}
-            pokemon["name"] = res["name"]
-            
-            pokemon["info"] = {}
-            pokemon["info"]["id"] = "#" + str(res["id"])
-            pokemon["info"]["exp"] = res["base_experience"]
-            pokemon["info"]["height"] = str(res["height"]) + '"'
-            pokemon["info"]["weight"] = str(res["weight"]) + "kg"
-
-            pokemon["types"] = []
-            n = len(res["types"])
-            for i in range(n):
-                type = res["types"][i]["type"]["name"]
-                pokemon["types"].append(type)
-
-            pokemon["stats"] = {}
-            n = len(res["stats"])
-            for i in range(n):
-                j = res["stats"][i]["stat"]["name"]
-                pokemon["stats"][j] = res["stats"][i]["base_stat"]
-
-            pokemon["sprite"] = res["sprites"]["other"]["dream_world"]["front_default"]
-            if pokemon["sprite"] == None:
-                pokemon["sprite"] = res["sprites"]["front_default"]
-        except:
-            pokemon = "Not Found"
+        pokemon = pokeapi.getData(name)
+        
     return render_template("pages/index.html", pokemon=pokemon, req=name)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5500)
+    app.run(port=5500)
---
+++ pokeapi.py
@@ -0,0 +1,37 @@
+import requests
+
+api = "https://pokeapi.co/api/v2"
+
+def getData(pokemon: str):
+    name = pokemon
+    url = api + "/pokemon/" + name
+    pokemon = {}
+    try:
+        req = requests.get(url)
+        res = req.json()
+        pokemon["name"] = res["name"]
+        
+        pokemon["info"] = {}
+        pokemon["info"]["id"] = "#" + str(res["id"])
+        pokemon["info"]["exp"] = res["base_experience"]
+        pokemon["info"]["height"] = str(res["height"]) + '"'
+        pokemon["info"]["weight"] = str(res["weight"]) + "kg"
+
+        pokemon["types"] = []
+        n = len(res["types"])
+        for i in range(n):
+            type = res["types"][i]["type"]["name"]
+            pokemon["types"].append(type)
+
+        pokemon["stats"] = {}
+        n = len(res["stats"])
+        for i in range(n):
+            j = res["stats"][i]["stat"]["name"]
+            pokemon["stats"][j] = res["stats"][i]["base_stat"]
+
+        pokemon["sprite"] = res["sprites"]["other"]["dream_world"]["front_default"]
+        if pokemon["sprite"] == None:
+            pokemon["sprite"] = res["sprites"]["front_default"]
+    except:
+        pokemon = "Not Found"
+    return pokemon
